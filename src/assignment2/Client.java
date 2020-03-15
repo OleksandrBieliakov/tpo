9cbb438f2115692ba59023757c1fc4922f3bf9b0
Oleksandr Bieliakov <abel9k@gmail.com>
2020-03-15 15:32:36 +0100

    Changed main in Client

--- src/assignment2/Client.java
+++ src/assignment2/Client.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Client {
@@ -12,6 +14,7 @@ public class Client {
     private static final int TIMES = 50;
     private static final int WAIT_MS = 200;
 
+    private static final int MIN_LEN = 3;
     private static final int MODE_INDEX = 0;
     private static final int HOST_INDEX = 1;
     private static final int PORT_INDEX = 2;
@@ -26,6 +29,9 @@ public class Client {
     private static final int NUM2_INDEX = 4;
 
     private static final String BYE = "BYE";
+
+    private final AddCommand addCommand = new AddCommand();
+    private final EchoCommand echoCommand = new EchoCommand();
 
     private Socket sock;
     private PrintWriter out;
@@ -100,7 +106,18 @@ public class Client {
     }
 
     public AddCommand getAddCommand() {
-        return new AddCommand();
+        return addCommand;
+    }
+
+    public class EchoCommand implements Command<String> {
+        @Override
+        public void execute(String data) {
+            echo(data);
+        }
+    }
+
+    public EchoCommand getEchoCommand() {
+        return echoCommand;
     }
 
     public void callCommand(Command command, Object data) {
@@ -110,55 +127,53 @@ public class Client {
     public static void main(String[] args) {
         int len = args.length;
 
-        if (len == ECHO_ARGS_LEN && args[MODE_INDEX].toUpperCase().equals(ECHO)) {
+        if (len >= MIN_LEN) {
+            int port = -1;
             try {
-                int port = Integer.parseInt(args[PORT_INDEX]);
-                String host = args[HOST_INDEX];
-
-                String message = args[MESSAGE_INDEX];
-
-                Client client = new Client(host, port);
-                for (int i = 0; i < TIMES; i++) {
-                    client.echo(message);
-
-                    //Command command = client.getAddCommand();
-                    //client.callCommand(command, args);
-
-                    try {
-                        Thread.sleep(WAIT_MS);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                client.bye();
-                client.close();
+                port = Integer.parseInt(args[PORT_INDEX]);
             } catch (NumberFormatException e) {
+                e.printStackTrace();
                 printExpectedArgumentsMessage();
             }
 
-        } else if (len == ADD_ARGS_LEN && args[MODE_INDEX].toUpperCase().equals(ADD)) {
-            try {
-                int port = Integer.parseInt(args[PORT_INDEX]);
-
-                int num1 = Integer.parseInt(args[NUM1_INDEX]);
-                int num2 = Integer.parseInt(args[NUM2_INDEX]);
-
+            if (port >= 0) {
+                String mode = args[MODE_INDEX].toUpperCase();
                 String host = args[HOST_INDEX];
                 Client client = new Client(host, port);
-                for (int i = 0; i < TIMES; i++) {
-                    client.add(num1, num2);
+
+                Command command = null;
+                Object data = null;
+                if (mode.equals(ECHO) && len == ECHO_ARGS_LEN) {
+                    data = args[MESSAGE_INDEX];
+                    command = client.getEchoCommand();
+                } else if (mode.equals(ADD) && len == ADD_ARGS_LEN) {
                     try {
-                        Thread.sleep(WAIT_MS);
-                    } catch (InterruptedException e) {
+                        int num1 = Integer.parseInt(args[NUM1_INDEX]);
+                        int num2 = Integer.parseInt(args[NUM2_INDEX]);
+                        data = Arrays.asList(num1, num2);
+                        command = client.getAddCommand();
+                    } catch (NumberFormatException e) {
                         e.printStackTrace();
+                        printExpectedArgumentsMessage();
+                    }
+                } else {
+                    printExpectedArgumentsMessage();
+                }
+
+                if (command != null) {
+                    for (int i = 0; i < TIMES; i++) {
+                        client.callCommand(command, data);
+                        try {
+                            Thread.sleep(WAIT_MS);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
+
                 client.bye();
                 client.close();
-            } catch (NumberFormatException e) {
-                printExpectedArgumentsMessage();
             }
-
         } else {
             printExpectedArgumentsMessage();
         }
